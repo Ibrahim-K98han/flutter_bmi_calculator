59f09e684ccb034b73bf6a3577e9bc5320ef20ad
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-01 13:52:40 +0600

    height text alignment center

--- lib/height_widget.dart
+++ lib/height_widget.dart
@@ -8,13 +8,27 @@ class HeightWidget extends StatefulWidget {
 }
 
 class _HeightWidgetState extends State<HeightWidget> {
+
+  int _height = 150;
+
   @override
   Widget build(BuildContext context) {
     return Card(
         elevation: 12,
         shape: RoundedRectangleBorder(),
     child: Column(
+      children: [
+        Text('Height', style:TextStyle(fontSize: 25, color: Colors.grey) ,),
+        SizedBox(height: 10,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_height.toString(),style: TextStyle(fontSize: 40),),
+            Text('cm',style: TextStyle(fontSize: 20, color: Colors.grey),),
+          ],
+        ),
 
+      ],
     ));
   }
 }
