945a724fcfdea8e7a4edf89bc72ec28933d6395a
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-01 14:01:47 +0600

    AgeWeightWidget class added

---
+++ lib/widgets/age_weight_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AgeWeightWidget extends StatefulWidget {
+  const AgeWeightWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AgeWeightWidget> createState() => _AgeWeightWidgetState();
+}
+
+class _AgeWeightWidgetState extends State<AgeWeightWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
